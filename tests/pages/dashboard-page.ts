f52d4ad9ed4dd84e9113d3086c02efb2c7b2232f
Enamulbd1 <enamulbd1.gmail.com>
2024-09-17 17:57:11 +0200

    Creating Bill

--- tests/pages/dashboard-page.ts
+++ tests/pages/dashboard-page.ts
@@ -1,31 +1,46 @@
-//dashboard-page.ts
 
-import { expect, type Locator, type Page } from '@playwright/test';
+import { expect, type Locator, type Page } from "@playwright/test";
 
 export class DashboardPage {
-  //Attributes
   readonly page: Page;
-  readonly redRoomViewButton: Locator;
-  readonly clientRoomViewButton: Locator;
-  readonly logoutButton: Locator; 
+  readonly pageHeading: Locator;
+  readonly roomsViewButton: Locator;
+  readonly clientsViewButton: Locator;
+  readonly billsViewButton: Locator;
+  readonly reservationsViewButton: Locator;
+  readonly logoutButton: Locator;
 
-  
+  // I used css selector in places with dynamic content.
   constructor(page: Page) {
     this.page = page;
-    this.redRoomViewButton = page.locator('.block:nth-child(1) > .btn')
-    this.clientRoomViewButton = page.locator('.block:nth-child(2) > .btn')
-    this.logoutButton = page.getByRole('button', { name: 'Logout' });
+    this.pageHeading = page.getByRole("heading", {
+      name: "Tester Hotel Overview",
+    });
+    this.roomsViewButton = page.locator(
+      "#app > div > div > div:nth-child(1) > a");
+    this.clientsViewButton = page.locator("#app > div > div > div:nth-child(2) > a");
+    this.billsViewButton = page.locator("#app > div > div > div:nth-child(3) > a");
+    this.reservationsViewButton = page.locator("#app > div > div > div:nth-child(4) > a");
+    this.logoutButton = page.getByRole("button", { name: "Logout" });
   }
 
-  async verifyRedRoomButton() {
-    await this.redRoomViewButton.isVisible()
+  async gotoRoomsView() {
+    await this.roomsViewButton.click();
   }
 
-  async verifyClientRoomButton() {
-    await this.clientRoomViewButton.isVisible()
+  async gotoClientsView() {
+    await this.clientsViewButton.click();
   }
 
-   async performLogout() {
+  async gotoBillsView() {
+    await this.billsViewButton.click();
+  }
+
+  async gotoReservationsView() {
+    await this.reservationsViewButton.click();
+  }
+
+  async performLogout() {
     await this.logoutButton.click();
   }
 }
--- tests/testsuite01.spec.ts
+++ tests/testsuite01.spec.ts
@@ -1,20 +1,200 @@
-//testsuite01.ts
 import { test, expect } from '@playwright/test';
 import { LoginPage } from './pages/login-page';
 import { DashboardPage } from './pages/dashboard-page';
-import { CreateNewClient } from './pages/createNewClient-pages';
+import { BillsPage } from './pages/bills-page';
+import { RoomsPage } from './pages/rooms-page';
+import { ClientsPage } from './pages/clients-page';
+import { CreateClientPage } from './pages/createClient-page';
+import { CreateBillPage } from './pages/createNewBill-page';
+import { ReservationsPage } from './pages/reservations-page';
+import { CreateRoomPage } from './pages/createRoom-page';
+
+
 
 test.describe('Test suite 01', () => {
-  test('Test case 01', async ({ page }) => {
+  
+   test('Test Case 01, Title overview ', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.performLogin(`${process.env.TEST_USERNAME}`,`${process.env.TEST_PASSWORD}`);
+  
+    const dashboardPage = new DashboardPage(page);
+    await expect(dashboardPage.pageHeading).toBeVisible();
+ 
+    await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
+    await page.waitForTimeout(2000);   
+
+   });
+
+  test('Tase case 02, creating a new room and asserting', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.performLogin(`${process.env.TEST_USERNAME}`,`${process.env.TEST_PASSWORD}`);
+    const dashboardPage = new DashboardPage(page);
+    const roomsPage = new RoomsPage(page);
+    const createRoomPage = new CreateRoomPage(page);
+    
+    await dashboardPage.gotoRoomsView();
+    await roomsPage.gotoCreateRoom();
+    await createRoomPage.createRoom();
+    await expect(roomsPage.page).toHaveURL(/.*rooms/);
+
+  });
+
+  test('Tase Case 03 : creating a new client and asserting', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.performLogin(`${process.env.TEST_USERNAME}`,`${process.env.TEST_PASSWORD}`);
+  
+    const dashboardPage = new DashboardPage(page);
+    const clientsPage = new ClientsPage(page);
+
+    await dashboardPage.gotoClientsView();
+    await clientsPage.gotoEditClient(0);
+    await clientsPage.goBack();
+    await expect(clientsPage.page).toHaveURL(/.*clients/);
+  });
+
+   test('Tase Case 04, creating the bill', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.performLogin(`${process.env.TEST_USERNAME}`,`${process.env.TEST_PASSWORD}`);
+    const dashboardPage = new DashboardPage(page);
+    const billsPage = new BillsPage(page);
+    const createBillPage = new CreateBillPage(page);
+  
+    await dashboardPage.gotoBillsView();
+    await billsPage.gotoCreateBill();
+
+    const randomCreateBill = await createBillPage.createBill();
+    const billRow = page.locator(`text=${randomCreateBill}`);
+    await expect(billRow).toBeVisible(); 
+
+  });
+
+ 
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* //testsuite01.ts
+import { test, expect } from '@playwright/test';
+import { LoginPage } from './pages/login-page';
+import { DashboardPage } from './pages/dashboard-page';
+import { CreatClientPage } from './pages/createClient-page';
+import { CreateBillPage} from './pages/createBill-page';
+import { CreateRoomPage } from './pages/createRoom-page';
+
+test.beforeEach(async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
+  await loginPage.performLogin(`${process.env.TEST_USERNAME}`,`${process.env.TEST_PASSWORD}`)
+  // await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
+
+  const dashboardPage = new DashboardPage(page);
+await expect(dashboardPage.pageHeading).toBeVisible();
+});
+
+test.afterEach(async ({ page }) => {
+  const dashboardPage = new DashboardPage(page);
+  await dashboardPage.performLogout();
+  await expect(dashboardPage.pageHeading).not.toBeVisible();
+});
+
+test.describe('Test suite 01', () => {
+  test('Test case 01 - Login Page', async ({ page }) => {
     const loginPage = new LoginPage(page);
     const dashboardPage = new DashboardPage(page);
 
-    await loginPage.goto();
-    await loginPage.performLogin(`${process.env.TEST_USERNAME}`,`${process.env.TEST_PASSWORD}`)
+    // await loginPage.goto();
+    // await loginPage.performLogin(`${process.env.TEST_USERNAME}`,`${process.env.TEST_PASSWORD}`)
     await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
+    await dashboardPage.performLogout();
+    await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible(); 
+    await page.waitForTimeout(2000);
+  });
+
+
+  test('Test case 02 - Create New Room', async ({ page }) => {
+    // const loginPage = new LoginPage(page);
+    const dashboardPage = new DashboardPage(page);
+    const createNewRoom2 = new CreateNewRoom2(page);
+
+     await loginPage.goto();
+     await loginPage.performLogin(`${process.env.TEST_USERNAME}`,`${process.env.TEST_PASSWORD}`)
+     await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible(); 
+
+    //asertions
+    await createNewRoom2.logToRoom();
+    await createNewRoom2.createRoom();
+
     await dashboardPage.performLogout();
     await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible(); 
     await page.waitForTimeout(5000);
 
   });
-});
+
+    test('Test case 03 Create New bills', async ({ page }) => {
+      const loginPage = new LoginPage(page);
+      const dashboardPage = new DashboardPage(page);
+      const createNewBills = new CreateNewbills(page);
+
+      await loginPage.goto();
+      await loginPage.performLogin(`${process.env.TEST_USERNAME}`,`${process.env.TEST_PASSWORD}`)
+      await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible(); 
+      await createNewBills.goToBills();
+
+      await createNewBills.goToCreateBills();
+      await createNewBills.saveBills();
+
+      const filledValue = await createNewBills.fillUpBillForm();
+      const element = page.locator('#app > div > div.bills > div:nth-last-child(1)')
+      
+      //Assertions
+      expect(element).toContainText('ID');
+      expect(element).toContainText(filledValue);
+      await dashboardPage.performLogout();
+      await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible(); 
+      await page.waitForTimeout(5000);
+
+    });
+
+      test('test case 04 - create new client', async ({ page }) =>{
+        const createNewClient = new CreateNewClient(page);
+        const element = page.locator(
+            "#app > div > div.clients > div:nth-last-child(1)");
+    
+        //asertions
+        await createNewClient.logToClient();
+        await createNewClient.createClient();
+        await expect(element).toContainText(createNewClient.fullname);
+        await expect(element).toContainText(createNewClient.emailaddress);
+        await expect(element).toContainText(createNewClient.telephonenummber);
+        await page.waitForTimeout(5000);
+    
+        });
+  });
+ */
